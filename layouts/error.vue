9f56ee0e775f62432b9447c915782f7a20a7af79
夏睿岑 <xiaruicen@xiayizhendeMacBook-Air.local>
2021-07-22 12:56:58 +0800

    errorinfo

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div
+  h1 對不起出錯啦～～
+  img(:src='src')
+</template>
+<script>
+export default{
+  props:['error'],
+  layout:'error_layout',
+  data() {
+    return {
+      src: 'https://mailbox.mnd.gov.tw/img/Error404-4_03.png' 
+    }
+}}
+</script>
---
+++ layouts/error_layout.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+div
+  header
+    h1 錯誤頁面
+  nuxt
+</template>
